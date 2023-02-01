aaa2c398b4791b042b8cc127a3a8f3f0da7c8011
Joe <joedpreece@gmail.com>
2023-02-01 21:21:06 +0000

    Changes

--- assets/js/importer.js
+++ assets/js/importer.js
@@ -15,9 +15,37 @@ $(function(){
 });
 
 $(function(){
+    $(".welcome_personal").load("files/welcome.html");
+});
+
+
+// Connected Leaders
+
+$(function(){
     $("#cl_timetable").load("files/connected_leaders/timetable.html");
 });
 
 $(function(){
     $("#cl_info").load("files/connected_leaders/info.html");
 });
+
+$(function(){
+    $("#cl_simulation").load("files/connected_leaders/simulation.html");
+});
+
+$(function(){
+    $("#cl_automation").load("files/connected_leaders/automation.html");
+});
+
+$(function(){
+    $("#cl_data").load("files/connected_leaders/data.html");
+});
+
+$(function(){
+    $("#cl_future_transport").load("files/connected_leaders/future_transport.html");
+});
+
+$(function(){
+    $("#cl_workforce").load("files/connected_leaders/workforce.html");
+});
+
