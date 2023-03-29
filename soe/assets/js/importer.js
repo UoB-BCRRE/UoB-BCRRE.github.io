3c8c71aded08f38511e38a0fae98cd0bac31eb2a
Joe <joedpreece@gmail.com>
2023-03-29 17:00:35 +0100

    Changes

--- soe/assets/js/importer.js
+++ soe/assets/js/importer.js
@@ -2,9 +2,13 @@ var path =  '/soe/content/'
 
 $(function(){
     $("#welcome").load(path + "welcome.html");
+    $("#events").load(path + "/events/events.html");
 });
 
 $(function(){
-    $("#staff_directory").load(path + "staff_directory.html");
+    $("#staff_directory1").load(path + "/staff_directory/floor_1.html");
+    $("#staff_directory2").load(path + "/staff_directory/floor_2.html");
+    $("#staff_directory3").load(path + "/staff_directory/floor_3.html");
+
 });
 
