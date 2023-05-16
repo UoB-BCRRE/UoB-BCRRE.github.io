5da5faff757301d2f14c48c7273580ce4a0f2370
Joe <joedpreece@gmail.com>
2023-05-16 14:32:18 +0100

    Changes

--- soe/assets/js/importer.js
+++ soe/assets/js/importer.js
@@ -1,7 +1,7 @@
 var path =  '/soe/content/'
 
 $(function(){
-    $("#welcome").load(path + "welcome.html");
+    $("#welcome").load(path + "/welcome/welcome.html");
     $("#events").load(path + "/events/events.html");
 });
 
@@ -9,6 +9,9 @@ $(function(){
     $("#staff_directory1").load(path + "/staff_directory/floor_1.html");
     $("#staff_directory2").load(path + "/staff_directory/floor_2.html");
     $("#staff_directory3").load(path + "/staff_directory/floor_3.html");
+});
 
+$(function(){
+    $("#events").load(path + "/events/events.html");
 });
 
